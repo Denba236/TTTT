f3e08a6878f04d9c9ab04dd2d368dca3174813c9
Denba236 <tvg96875@gmail.com>
2025-07-24 13:42:30 +0200

    Update add_player.php

--- add_player.php
+++ add_player.php
@@ -3,6 +3,6 @@ $name = $_POST["name"];
 $points = intval($_POST["points"]);
 $ranking = json_decode(file_get_contents("ranking.json"), true);
 $ranking[] = ["name" => $name, "points" => $points];
-file_put_contents("ranking.json", json_encode($ranking, JSON_PRETTY_PRINT));
+file_put_contents("ranking.txt", json_encode($ranking, JSON_PRETTY_PRINT));
 header("Location: ranking.html");
 ?>
